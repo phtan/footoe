79f336058135e559f2084d98a3c276030e4f3fac
Pheng Heong, Tan <phtan90@gmail.com>
2020-06-21 20:05:53 +0800

    Set up file-structures, or suitable directories for this project.
    I am following a tutorial I found at the following URL:
    https://towardsdatascience.com/build-your-first-open-source-python-project-53471c9942a7

---
+++ footoe/footoe.py
@@ -0,0 +1,11 @@
+def convert(your_text):
+    """
+    Print a line about converting text
+    Args:
+        your_text (str): a certain text
+    Returns:
+        None
+
+    """
+    print(f"Hello world, I plan to convert the following text: 
+    {your_text}")    
