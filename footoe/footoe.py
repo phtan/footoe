886f951e74e524b18d5eb76c40b95aa31e0feef4
Pheng Heong, Tan <phtan90@gmail.com>
2020-06-29 11:12:57 +0800

    Update footoe.py
    I lay out a rough plan, in the function called main(). Just in case...

--- footoe/footoe.py
+++ footoe/footoe.py
@@ -1,5 +1,9 @@
 import sys
 
+def getTextFromFile():
+    file_name = sys.argv[1] # naively
+  # TODO
+  
 def convert(your_text):
     """
     Print a line about converting text
@@ -10,11 +14,15 @@ def convert(your_text):
 
     """
     print(f"Hello world, I plan to convert the following text: {your_text}")    
-    
+
+def writeToFile(converted_text):
+     # TODO
+    print("hello, here's a TODO")
     
 def main():
-     list_of_some_text = sys.argv[1:] # naively get input, for now
-     convert(list_of_some_text) # this argument is not the expected type, a str
-
-if __name__ == "__main__":
+    text = getTextFromFile()
+    new_text = convert(text)
+    writeToFile(new_text)
+    
+if __name__ == "__main__": # we want main() to run if called from command-line
     main()
