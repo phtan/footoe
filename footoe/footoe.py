7555795bdaec27cce31712d2b7ed5e96dff5e1b6
Pheng Heong, Tan <phtan90@gmail.com>
2020-06-29 10:37:16 +0800

    Update footoe.py
    I have the beginnings of a command-line program!

--- footoe/footoe.py
+++ footoe/footoe.py
@@ -1,3 +1,5 @@
+import sys
+
 def convert(your_text):
     """
     Print a line about converting text
@@ -8,3 +10,11 @@ def convert(your_text):
 
     """
     print(f"Hello world, I plan to convert the following text: {your_text}")    
+    
+    
+def main():
+     list_of_some_text = sys.argv[1:] # naively get input, for now
+     convert(list_of_some_text) # this argument is not the expected type, a str
+
+if __name__ == "__main__":
+    main()
